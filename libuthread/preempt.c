f72386d909f2a29e4e5d577f61db2a0d26b6415b
Brmanzo <76194522+Brmanzo@users.noreply.github.com>
2022-11-03 23:47:16 -0700

    formatting

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -23,54 +23,54 @@ sigset_t block_mask;
 struct itimerval timer_setup;
 
 static void sig_handler() {
-	if (disable_flag) {
-		return;
-	}
-	else {
-		setitimer(SIGVTALRM, &timer_setup, NULL);
-		uthread_yield();
-	}
+  if (disable_flag) {
+    return;
+  }
+  else {
+    setitimer(SIGVTALRM, &timer_setup, NULL);
+    uthread_yield();
+  }
 }
 
 void preempt_disable(void)
 {
-	disable_flag = 1;
+  disable_flag = 1;
 }
 
 void preempt_enable(void)
 {
-	disable_flag = 0;
-	setitimer(SIGVTALRM, &timer_setup, NULL);
+  disable_flag = 0;
+  setitimer(SIGVTALRM, &timer_setup, NULL);
 }
 
 void preempt_start(bool preempt)
-{
-	
-	if (!preempt) {
-		return;
-	}
-	else {
-		sigemptyset(&block_mask);
-		sig_setup.sa_handler = sig_handler;
-		sig_setup.sa_mask = block_mask;
-		/* If flag is set, returning from a handler resumes the library function primitives (open, read, write). */
-		sig_setup.sa_flags = SA_RESTART;
-		sigaction(SIGVTALRM, &sig_setup, NULL);
+{	
+  if (!preempt) {
+    return;
+  }
+  else {
+    sigemptyset(&block_mask);
+    sig_setup.sa_handler = sig_handler;
+    sig_setup.sa_mask = block_mask;
+    /* If flag is set, returning from a handler resumes the */
+    /* library function primitives (open, read, write).     */
+    sig_setup.sa_flags = SA_RESTART;
+    sigaction(SIGVTALRM, &sig_setup, NULL);
 
-		/* u in usec stands for micro- */
-		/* Period between successive timer attempts. If zero, the alarm will only be sent once. */
-		timer_setup.it_interval.tv_sec = 0;
-		timer_setup.it_interval.tv_usec = (1000000 / HZ);
-		/* Period between now and first timer interrupt. If zero, the alarm is disabled. */
-		timer_setup.it_value.tv_sec = 0;
-		timer_setup.it_value.tv_usec = (1000000 / HZ);
+    /* u in usec stands for micro- */
+    /* Period between successive timer attempts. If zero, the alarm will only be sent once. */
+    timer_setup.it_interval.tv_sec = 0;
+    timer_setup.it_interval.tv_usec = (1000000 / HZ);
+    /* Period between now and first timer interrupt. If zero, the alarm is disabled. */
+    timer_setup.it_value.tv_sec = 0;
+    timer_setup.it_value.tv_usec = (1000000 / HZ);
 
-		setitimer(ITIMER_VIRTUAL, &timer_setup, NULL);
-	}
+    setitimer(ITIMER_VIRTUAL, &timer_setup, NULL);
+  }
 }
 
 void preempt_stop(void)
 {
-	preempt_disable();
-	signal(SIGVTALRM, SIG_DFL);
+  preempt_disable();
+  signal(SIGVTALRM, SIG_DFL);
 }
